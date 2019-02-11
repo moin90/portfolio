ec49176ebdd287ff580c83681644bc29f37bf7eb
Moin <moin.g4@gmail.com>
2019-02-10 20:47:09 -0500

    uploaded most recent version of my portfolio with fixed resume button

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,2 +1,32 @@
-const hello = 'World';
-console.log(hello);
+import Typed from 'typed.js';
+import AOS from 'aos';
+
+$(document).ready(function () {
+
+    $('a').smoothScroll({
+        speed: 600
+    });
+
+    $("#card1, #card2, #card3, #card4, #card5, #card6, #card7, #card8, #card9").flip({
+        trigger: 'hover',
+    });
+
+    $('.hamburger').on('click', function () {
+        $('.menu').addClass('show');
+    });
+
+    $('.exit-responsive').on('click', function () {
+        $('.menu').removeClass('show');
+    });
+});
+
+var typed = new Typed('.typed', {
+    strings: ["Front-end developer.", "Diehard Leafs fan.", "Travel junkie."],
+    typeSpeed: 30,
+    backSpeed: 30,
+    loop: true,
+    loopCount: Infinity,
+    showCursor: false
+});
+
+AOS.init();
